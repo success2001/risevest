e1dfd7849259d8bdee1131d23e83fc16ea75b170
success2001 <50456303+success2001@users.noreply.github.com>
2022-02-11 00:37:21 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,23 @@
 import './App.css';
 import {NavLink, Routes, Route, Link } from 'react-router-dom';
 import React, {useState}  from "react";
-
 import IconButton from "@material-ui/core/IconButton";
 import InputLabel from "@material-ui/core/InputLabel";
 import Visibility from "@material-ui/icons/Visibility";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Input from "@material-ui/core/Input";
-
 import {useEffect} from "react";
-// import login from './login';
 function App() {
   const[errorMessages, setErrorMessages] = useState({});
   const[isSubmitted, setisSubmitted] = useState(false);
   const[passwordShown, setPasswordShown] = useState(false);
-  
-
   const [values, setValues] = React.useState({
     password: "",
     showPassword: false,
   });
-  
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
+   const handleClickShowPassword = () => {
+   setValues({ ...values, showPassword: !values.showPassword });
   };
   
   const handleMouseDownPassword = (event) => {
@@ -34,9 +28,7 @@ function App() {
   const handlePasswordChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
-  
- 
-  const database = [
+   const database = [
     {
       email: "pope@gmail.com",
       password:"Pope2001"
@@ -50,73 +42,53 @@ function App() {
       password:"User2001"
     },
   ];
- 
-  const errors = 
+   const errors = 
     {
       mail: "invalid mail",
       pass:"invalid password"
     };
-
-
-
-
-
-  const handleSubmit = (event) => {
-
-    event.preventDefault();
+ const handleSubmit = (event) => {
+  event.preventDefault();
       var {mail,pass} = document.forms[0];
-      
-      const userData = database.find((user) => user.email === mail.value)
+       const userData = database.find((user) => user.email === mail.value)
       if (userData){
         if (userData.password !==pass.value){
           setErrorMessages({name: "pass", message:errors.pass});
         }else {setisSubmitted(true);
-         
-        }
+           }
       }else{
         setErrorMessages({
           name: "mail", message: errors.mail
         });
       }
   };
-
-  const togglePassword = () => {
+ const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
-
-  const renderErrorMessage = (name) =>
+ const renderErrorMessage = (name) =>
   name === errorMessages.name && (
   <div className="error">{errorMessages.message}</div>
 
   );
-  
-
-  const renderForm = (
+    const renderForm = (
     <div className="Flex">
-      
-      
-    <form onSubmit={handleSubmit}  >
+       <form onSubmit={handleSubmit}  >
       <div className="sign-in">Sign in</div>
       <div>
-     
         <input type="email" 
         name = "mail"
         placeholder="Email address"
        required/>
        {renderErrorMessage("mail")}
      </div> 
-
-
      <div className="passwords">
      <Input  style = {{textDecoration:"none",color:"white",fontSize:"small"}}
         type={values.showPassword ? "text" : "password"}
-
-        placeholder="Password"
+   placeholder="Password"
         name ="pass"
         onChange={handlePasswordChange("password")}
         value={values.password}
-        
-        endAdornment={
+              endAdornment={
           <InputAdornment position="end">
             <IconButton 
               onClick={handleClickShowPassword}
@@ -125,38 +97,25 @@ function App() {
               {values.showPassword ? <Visibility /> : <VisibilityOff />}
             </IconButton>
             </InputAdornment>
-        
         }
       />
       {renderErrorMessage("pass")}
      </div>
-
-   
-  
-
 <div> <input type="checkbox" value="check" 
       placeholder="rememberMe" />
       <font color="white">Remember Me</font></div>
-    
-     <div className="Sign in"> 
-     
-      <input type="submit" value ="SIGN IN"/ >
+       <div className="Sign in"> 
+       <input type="submit" value ="SIGN IN"/ >
       </div>
-      
-      <div><Link to ="/forgotpassword" style = {{textDecoration:"none",color:"white",fontSize:"small"}}>I forgot my password</Link></div>
-     
-    </form>
+         <div><Link to ="/forgotpassword" style = {{textDecoration:"none",color:"white",fontSize:"small"}}>I forgot my password</Link></div>
+       </form>
      </div>
-    
-  )
+     )
   return (
-    
-    <div className="App">
+       <div className="App">
     <body> 
-     
-      <h1 style ={{float:"left"}}><span>money</span>rise</h1>
-      
-      <Routes>
+       <h1 style ={{float:"left"}}><span>money</span>rise</h1>
+        <Routes>
         <Route path="login" element={<login/>} />
         <Route path="contact" element={<contact/>} />
       </Routes>
@@ -168,24 +127,13 @@ function App() {
     <div class="footer"> if you find difficulty accessing your account, Get help <Link to ="/help" style = {{color:"white"}}>here</Link>
     </div>
     </body>
-   
-   
-   
 <div className="login-form">
-
-<div>
+  <div>
   {isSubmitted ? <div className="logged-in">User is successfully logged in</div>:renderForm}
 </div>
 </div>
-
-    </div>
+  </div>
   );
-
- 
-
-
 }
-
-
 export default App;
 
